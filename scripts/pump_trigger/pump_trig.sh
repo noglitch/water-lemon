10b37d09a018017fb6b0b92b8275554bb90f38f8
Nicolas Ferre <noglitch@free.fr>
2017-08-10 18:26:04 +0200

    pump_trig.sh: add support for watering signal file
    The file is positioned by the sensor application.
    Only water plants if the signal file is present. Remove the signal when
    watering plants.
    
    Signed-off-by: Nicolas Ferre <noglitch@free.fr>

--- scripts/pump_trigger/pump_trig.sh
+++ scripts/pump_trigger/pump_trig.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+SIGNAL_FILE_PATH=/var/run/water-lemon
+SIGNAL_FILENAME=do_water_lemontree.tmp
 PUMP_ACT_PATH=/root/pump_trigger
 
-echo "pump cycle start"
+function test_signal_present() {
+	if [ -f "${SIGNAL_FILE_PATH}/${SIGNAL_FILENAME}" ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
 date
+echo -n "start the pump? "
+test_signal_present
+if [ $? -eq 1 ]; then
+	# remove the signal
+	rm -f "${SIGNAL_FILE_PATH}/${SIGNAL_FILENAME}"
+	echo "pump cycle start..."
+else
+	echo "no!"
+	exit 0
+fi
+
 for i in $(seq 1 4); do
 	${PUMP_ACT_PATH}/pump_activate.sh on
 	sleep 25
