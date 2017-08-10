e5aeed8da77a0934f36395dffbc4e21dcd47c641
Nicolas Ferre <noglitch@free.fr>
2017-08-10 17:19:42 +0200

    test_adafruitio: add file functions for the signal
    A file signal is used for telling the bash script to trigger the
    watering pump. Add the functions to manage this signal.
    
    The signal is sent if a minimum value for the humidity sensor
    is reached.
    
    Signed-off-by: Nicolas Ferre <noglitch@free.fr>

--- test_adafruitio.py
+++ test_adafruitio.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
 
-import time, sys
+import time, sys, os
 
 from chirp import Chirp
 from Adafruit_IO import Client
 from math import log10
+from datetime import datetime
 
 aio = Client('some_customer_id_here')
+filedir = "/var/run/water-lemon"
+filename = filedir + "/do_water_lemontree.tmp"
+
+def remove_signal():
+    try:
+        # if file exists, delete it
+        if os.path.isfile(filename):
+            os.unlink(filename)
+    except:
+        print "Error: cannot remove signal file"
+
+def water_plant_signal():
+    try:
+        f = open(filename, 'a')
+        dt = datetime.now().isoformat()
+        f.write(dt)
+        f.write('\n')
+        f.close()
+    except:
+        print "Error: cannot write signal file"
 
 if __name__ == "__main__":
     addr = 0x20
@@ -15,6 +36,8 @@ if __name__ == "__main__":
             addr = int(sys.argv[1], 16)
         else:
             addr = int(sys.argv[1])
+
+    remove_signal()
     chirp = Chirp(2, addr)
 
     print chirp
@@ -23,6 +46,8 @@ if __name__ == "__main__":
         val=chirp.temp()
         time.sleep(0.2)
         hval=chirp.cap_sense()
+        if hval < 415:
+            water_plant_signal()
         time.sleep(0.2)
         lval=chirp.light();
         print "%d\t%d\t%d" % (hval, val, lval)
